501fb147472243735cd20c70c4ef8938b37b7d99
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-09-07 13:01:48 +0200

    Refactor absents function

--- lib/puppet/parser/functions/absents.rb
+++ lib/puppet/parser/functions/absents.rb
@@ -1,11 +1,9 @@
-module Puppet::Parser::Functions
-  newfunction(:absents, :type => :rvalue, :doc => <<-EOS
-      Returns a hash of the elements that have ensure == absent.
-    EOS
-  ) do |args|
-    raise(Puppet::ArgumentError, "absents(): Wrong number of arguments given") if args.size != 1
-    raise(Puppet::ArgumentError, "absents(): First parameter must be a hash") if args[0].class != Hash
+Puppet::Parser::Functions.newfunction(:absents, :type => :rvalue, :doc => <<-'ENDHEREDOC') do |args|
+  Returns a hash of the elements that have ensure == absent.
+ENDHEREDOC
 
-    Hash[args[0].select{ |k, v| v['ensure'] == 'absent' }]
-  end
+  raise(Puppet::ArgumentError, "absents(): Wrong number of arguments given") if args.size != 1
+  raise(Puppet::ArgumentError, "absents(): First parameter must be a hash") if args[0].class != Hash
+
+  Hash[args[0].select{ |k, v| v['ensure'] == 'absent' }]
 end
